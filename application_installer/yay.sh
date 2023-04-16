5339bc5d4251b4667bf66d8b6a11a54632c6a715
Ashdevelopernewbie <aahashish100@gmail.com>
2023-04-16 16:07:19 +0545

    refactor: Language and style refactors.

--- application_installer/yay.sh
+++ application_installer/yay.sh
@@ -1,22 +1,18 @@
 #!/bin/bash
-#
-echo "Do you want to install yay package manager for arch based systems?(yes/no)"
-read -r ans
+
+echo ""
+echo "[.] Installing yay package manager for arch based systems..."
 
 yay_installer() {
-	echo "This process will require sudo priviledges."
+	echo "[.] This process will require sudo priviledges."
 	sleep .5
 	sudo pacman -S --needed base-devel git || true
-	cd ~/Downloads
+	cd ~/Downloads || return
 	git clone https://aur.archlinux.org/yay-git.git || true
 	cd yay-git && makepkg -sri || return
 }
 
-if [[ "$ans" == "yes" ]]; then
-	SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-	yay_installer
-	yay --version
-else
-	echo "Have a good day!"
-	exit 1
-fi
+SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)
+cd "$SCRIPT_DIR" || return
+yay_installer
+yay --version
