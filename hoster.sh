4e36c4141ae49c24ce6dde346cca4493af11ca90
duckCuddlerTadashi <41185494+Ashdevelopernewbie@users.noreply.github.com>
2023-05-29 14:21:46 +0545

    Update hoster.sh

--- hoster.sh
+++ hoster.sh
@@ -6,19 +6,19 @@ os=$(uname)
 if [[ "$os" == "Linux" ]]; then
     # Add the line to /etc/hosts
     echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-    echo "Added the line to /etc/hosts."
+#     echo "Added the line to /etc/hosts."
 elif [[ "$os" == "Darwin" ]]; then
     # Add the line to /etc/hosts
     echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-    echo "Added the line to /etc/hosts."
+#     echo "Added the line to /etc/hosts."
 elif [[ "$os" == "FreeBSD" ]]; then
     # Add the line to /etc/hosts
     echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-    echo "Added the line to /etc/hosts."
+#     echo "Added the line to /etc/hosts."
 elif [[ "$os" == "MINGW"* ]]; then
     # Add the line to C:\Windows\System32\drivers\etc\hosts
     echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null
-    echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
+#     echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
 else
     echo "Unsupported operating system."
     exit 1
