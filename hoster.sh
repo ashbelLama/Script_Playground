729ed1793b5eb79bda2004f2ea9b6e1a982bc70d
duckCuddlerTadashi <41185494+Ashdevelopernewbie@users.noreply.github.com>
2023-05-29 14:24:13 +0545

    Update hoster.sh

--- hoster.sh
+++ hoster.sh
@@ -5,20 +5,36 @@ os=$(uname)
 
 if [[ "$os" == "Linux" ]]; then
     # Add the line to /etc/hosts
-    echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-#     echo "Added the line to /etc/hosts."
+    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
+        echo "Added the line to /etc/hosts."
+    else
+        echo "Failed to add the line to /etc/hosts."
+        exit 1
+    fi
 elif [[ "$os" == "Darwin" ]]; then
     # Add the line to /etc/hosts
-    echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-#     echo "Added the line to /etc/hosts."
+    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
+        echo "Added the line to /etc/hosts."
+    else
+        echo "Failed to add the line to /etc/hosts."
+        exit 1
+    fi
 elif [[ "$os" == "FreeBSD" ]]; then
     # Add the line to /etc/hosts
-    echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null
-#     echo "Added the line to /etc/hosts."
+    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
+        echo "Added the line to /etc/hosts."
+    else
+        echo "Failed to add the line to /etc/hosts."
+        exit 1
+    fi
 elif [[ "$os" == "MINGW"* ]]; then
     # Add the line to C:\Windows\System32\drivers\etc\hosts
-    echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null
-#     echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
+    if echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null; then
+        echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
+    else
+        echo "Failed to add the line to C:\Windows\System32\drivers\etc\hosts."
+        exit 1
+    fi
 else
     echo "Unsupported operating system."
     exit 1
