21cf55b087bfad2a3ed1f711c87a5c9e4c6c38df
duckCuddlerTadashi <41185494+Ashdevelopernewbie@users.noreply.github.com>
2023-05-29 14:27:26 +0545

    Add files via upload

--- hoster.sh
+++ hoster.sh
@@ -1,41 +1,27 @@
 #!/bin/bash
 
-# Check the user's operating system
-os=$(uname)
+# Function to add entry to /etc/hosts file
+add_entry_to_hosts() {
+    echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts > /dev/null
+    echo "Entry added to /etc/hosts"
+}
 
-if [[ "$os" == "Linux" ]]; then
-    # Add the line to /etc/hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
-    else
-        echo "Failed to add the line to /etc/hosts."
-        exit 1
-    fi
-elif [[ "$os" == "Darwin" ]]; then
-    # Add the line to /etc/hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
-    else
-        echo "Failed to add the line to /etc/hosts."
-        exit 1
-    fi
-elif [[ "$os" == "FreeBSD" ]]; then
-    # Add the line to /etc/hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
-    else
-        echo "Failed to add the line to /etc/hosts."
-        exit 1
-    fi
-elif [[ "$os" == "MINGW"* ]]; then
-    # Add the line to C:\Windows\System32\drivers\etc\hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null; then
-        echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
-    else
-        echo "Failed to add the line to C:\Windows\System32\drivers\etc\hosts."
-        exit 1
-    fi
-else
-    echo "Unsupported operating system."
-    exit 1
-fi
+# Check the operating system
+os=$(uname -s)
+
+# Add entry to /etc/hosts based on the operating system
+case $os in
+    Linux*)
+        add_entry_to_hosts
+        ;;
+    Darwin*)
+        add_entry_to_hosts
+        ;;
+    CYGWIN*|MINGW32*|MSYS*|MINGW*)
+        # Running on Windows
+        echo "This script does not support Windows"
+        ;;
+    *)
+        echo "Unsupported operating system"
+        ;;
+esac
