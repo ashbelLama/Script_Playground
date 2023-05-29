dae33fb758307224291349ef5fd39326cceb3e89
duckCuddlerTadashi <41185494+Ashdevelopernewbie@users.noreply.github.com>
2023-05-29 14:18:40 +0545

    Update hoster.sh

--- hoster.sh
+++ hoster.sh
@@ -6,36 +6,36 @@ os=$(uname)
 if [[ "$os" == "Linux" ]]; then
     # Add the line to /etc/hosts
     if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
+#         echo "Added the line to /etc/hosts."
     else
         echo "Failed to add the line to /etc/hosts."
         exit 1
     fi
-elif [[ "$os" == "Darwin" ]]; then
-    # Add the line to /etc/hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
-    else
-        echo "Failed to add the line to /etc/hosts."
-        exit 1
-    fi
-elif [[ "$os" == "FreeBSD" ]]; then
-    # Add the line to /etc/hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
-        echo "Added the line to /etc/hosts."
-    else
-        echo "Failed to add the line to /etc/hosts."
-        exit 1
-    fi
-elif [[ "$os" == "MINGW"* ]]; then
-    # Add the line to C:\Windows\System32\drivers\etc\hosts
-    if echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null; then
-        echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
-    else
-        echo "Failed to add the line to C:\Windows\System32\drivers\etc\hosts."
-        exit 1
-    fi
-else
-    echo "Unsupported operating system."
-    exit 1
+# elif [[ "$os" == "Darwin" ]]; then
+#     # Add the line to /etc/hosts
+#     if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
+#         echo "Added the line to /etc/hosts."
+#     else
+#         echo "Failed to add the line to /etc/hosts."
+#         exit 1
+#     fi
+# elif [[ "$os" == "FreeBSD" ]]; then
+#     # Add the line to /etc/hosts
+#     if echo "192.168.0.240    inctic.wiriga.local" | sudo tee -a /etc/hosts >/dev/null; then
+#         echo "Added the line to /etc/hosts."
+#     else
+#         echo "Failed to add the line to /etc/hosts."
+#         exit 1
+#     fi
+# elif [[ "$os" == "MINGW"* ]]; then
+#     # Add the line to C:\Windows\System32\drivers\etc\hosts
+#     if echo "192.168.0.240    inctic.wiriga.local" | tee -a /mnt/c/Windows/System32/drivers/etc/hosts >/dev/null; then
+#         echo "Added the line to C:\Windows\System32\drivers\etc\hosts."
+#     else
+#         echo "Failed to add the line to C:\Windows\System32\drivers\etc\hosts."
+#         exit 1
+#     fi
+# else
+#     echo "Unsupported operating system."
+#     exit 1
 fi
